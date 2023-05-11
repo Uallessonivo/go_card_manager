ab2137cb15c1bd18734e6ea43d3566fd600f8a32
Uallessonivo <uallessons@gmail.com>
2023-05-11 15:42:29 -0300

    wip(application): extracting data from excel file

--- application/utils/extract_data_file.go
+++ application/utils/extract_data_file.go
@@ -1,11 +1,37 @@
 package utils
 
 import (
+	"github.com/Uallessonivo/go_card_manager/domain/enums"
 	"github.com/Uallessonivo/go_card_manager/domain/model"
 	"github.com/xuri/excelize/v2"
 )
 
-// TOOD: extract data from file
+// WIP: extract data from file
 func ExtractDataFromExcelFile(file *excelize.File) ([]*model.CardRequest, error) {
-	return nil, nil
+	var cards []*model.CardRequest
+
+	rows, err := file.GetRows("Results")
+	if err != nil {
+		return nil, err
+	}
+
+	cardTypes := map[string]enums.CardType{
+		"MATRIZ": enums.DespesasMatriz,
+		"FILIAL": enums.DespesasFilial,
+	}
+
+	for i, row := range rows {
+		if i < 1 {
+			continue
+		}
+
+		cards = append(cards, &model.CardRequest{
+			Type:   cardTypes[row[4]],
+			Serial: row[1],
+			Owner:  row[2],
+		})
+		println(row)
+	}
+
+	return cards, nil
 }
