9de6df9b4f552e15d395ff38dbb041fead1374b0
Uallessonivo <uallessons@gmail.com>
2024-12-02 16:42:31 -0300

    feat(card): format card type constants for consistency

--- internal/core/domain/enums/card_type.go
+++ internal/core/domain/enums/card_type.go
@@ -4,10 +4,10 @@ type CardType string
 
 const (
 	DespesasMatriz CardType = "DESPESAS_MATRIZ"
-	DespesasFilial          = "DESPESAS_FILIAL"
-	IncentivoCap1           = "INCENTIVO_CAP1"
-	IncentivoCap2           = "INCENTIVO_CAP2"
-	IncentivoInt1           = "INCENTIVO_INT1"
-	IncentivoInt2           = "INCENTIVO_INT2"
-	IncentivoSe             = "INCENTIVO_SE"
+	DespesasFilial CardType = "DESPESAS_FILIAL"
+	IncentivoCap1  CardType = "INCENTIVO_CAP1"
+	IncentivoCap2  CardType = "INCENTIVO_CAP2"
+	IncentivoInt1  CardType = "INCENTIVO_INT1"
+	IncentivoInt2  CardType = "INCENTIVO_INT2"
+	IncentivoSe    CardType = "INCENTIVO_SE"
 )
