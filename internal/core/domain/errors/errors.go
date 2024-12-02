80dfe19e9bc1d3bb12bbd59534c138e65b3e9b8b
Uallessonivo <uallessons@gmail.com>
2024-12-02 16:42:38 -0300

    feat(errors): add error message for invalid card type

--- internal/core/domain/errors/errors.go
+++ internal/core/domain/errors/errors.go
@@ -12,6 +12,7 @@ var (
 	InvalidPassword  = errors.New("password must be alphanumeric and must consists of at least 6 characters and not more than 15 characters")
 	InvalidParams    = errors.New("please make sure all required fields are filled out and try again")
 	InvalidFields    = errors.New("one or more fields in the data you sent are invalids")
+	InvalidCardType  = errors.New("invalid card type")
 	MaxNumberOfCards = errors.New("each employee must have a maximum of 2 cards")
 	FileExtension    = errors.New("we only support '.xlsx' files")
 	InvalidLogin     = errors.New("email or Password invalid")
