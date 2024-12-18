f1a1d05b598acc7984633a8681b42c75b8065ca7
Uallessonivo <uallessons@gmail.com>
2024-12-17 21:13:48 -0300

    feat(tests): implement MockCardService with methods for card operations

--- tests/services/mocks_service_test.go
+++ tests/services/mocks_service_test.go
@@ -13,6 +13,40 @@ type MockEmployeeRepository struct {
 	mock.Mock
 }
 
+type MockCardService struct {
+	mock.Mock
+}
+
+func (m *MockCardService) CreateCard(input *models.CardRequest) (*models.CardResponse, error) {
+	args := m.Called(input)
+	return args.Get(0).(*models.CardResponse), args.Error(1)
+}
+
+func (m *MockCardService) DeleteCard(id string) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+func (m *MockCardService) ListAllCards() ([]*models.CardResponse, error) {
+	args := m.Called()
+	return args.Get(0).([]*models.CardResponse), args.Error(1)
+}
+
+func (m *MockCardService) ListAllCardsByOwner(input string) ([]*models.CardResponse, error) {
+	args := m.Called(input)
+	return args.Get(0).([]*models.CardResponse), args.Error(1)
+}
+
+func (m *MockCardService) ListAllCardsByType(input string) ([]*models.CardResponse, error) {
+	args := m.Called(input)
+	return args.Get(0).([]*models.CardResponse), args.Error(1)
+}
+
+func (m *MockCardService) ValidateCard(input string) (*models.Employee, error) {
+	args := m.Called(input)
+	return args.Get(0).(*models.Employee), args.Error(1)
+}
+
 func (m *MockEmployeeRepository) List() ([]*models.Employee, error) {
 	args := m.Called()
 	return args.Get(0).([]*models.Employee), args.Error(1)
